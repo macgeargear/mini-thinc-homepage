0e752c10f8a6110c493f90c8898d37a9330ef793
macgeargear <macgeargear@gmail.com>
2022-09-12 19:12:04 +0700

    thinc home page

---
+++ app.ts
@@ -0,0 +1,12 @@
+interface Course {
+  courseNo: string;
+  abbrName: string;
+  courseNameTh: string;
+  courseNameEn: string;
+  department: string;
+  credit: number;
+  creditHours: string;
+  genEdType: string;
+  updatedAt: string;
+}
+
